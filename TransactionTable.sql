8606473341eca13fa090400b0e3c8d4f1d4bb6a7
ThyRex3 <kathydai@email.uark.edu>
2015-09-28 09:47:36 -0500

    Create TransactionTable.sql

---
+++ TransactionTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Transaction{
+	Record_ID uuid PRIMARY KEY,
+	Cashier_ID uuid  NOT NULL,
+	Foreign Key (Cashier_ID) References Employee(Record_ID),
+	Amount decimal(5,2) NOT NULL,
+	TransactionType varchar(25) check (Transaction in ('sale', 'return', 'void')),
+	Parent_ID uuid,
+	Foreign Key (Parent_ID) References Transaction (Record_ID),
+	Time_Stamp timestamp without time zone NOT NULL DEFAULT now()
+};
